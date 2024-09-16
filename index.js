d29ad2207713d89d3c34d522041df0d74276e2f1
Doanh-Nguyen1207 <105596379+Doanh-Nguyen1207@users.noreply.github.com>
2024-09-17 00:27:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ stdin.on('data', function (n) {
     }
     process.exit()
 })
+
+//Or maybe use this
+
+process.stdin.on("data", n => {
+    n = n * 1
+    console.log('n: ', n)
+    for (let i = 0; i <= n; i++) {
+        const arr = []
+        for (let j = 0; j <= i; j++) {
+            arr.push('x ')
+        }
+        console.log(arr.join('') + '\n')
+    }
+    process.exit()
+})
